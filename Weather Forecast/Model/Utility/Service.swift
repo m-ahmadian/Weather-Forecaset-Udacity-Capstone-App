c460b41b6933829d574083c39c59049eb6d8d567
Mehrdad Ahmadian <mehrdad.ahmadian@gmail.com>
2021-09-06 00:31:50 -0400

    feat: Add a generic GET request class func

--- Weather Forecast/Model/Utility/Service.swift
+++ Weather Forecast/Model/Utility/Service.swift
@@ -37,4 +37,30 @@ class Service {
             return URL(string: stringValue)!
         }
     }
+
+    // A GET request class fnc with generics and discardableResult attribute for the return type if it's not beiing used
+    @discardableResult class func taskForGETRequest<ResponseType: Decodable>(url: URL, response: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) -> URLSessionTask {
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let response = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(response, nil)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+        }
+        task.resume()
+        return task
+    }
 }
