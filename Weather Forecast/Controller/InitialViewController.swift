ea34fb2f07185af7a838178d36a901e015525b8b
Mehrdad Ahmadian <mehrdad.ahmadian@gmail.com>
2021-09-06 00:01:48 -0400

    feat: Prepare the UI

---
+++ Weather Forecast/Controller/InitialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InitialViewController.swift
+//  Weather Forecast
+//
+//  Created by Mehrdad Ahmadian on 2021-09-05.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
