b1bfaa04bb68e4ac1f27988a983261356d2dd187
Mehrdad Ahmadian <mehrdad.ahmadian@gmail.com>
2021-09-06 17:52:29 -0400

    feat: Add a struct for City Weather response

---
+++ Weather Forecast/Model/CityWeatherResponse.swift
@@ -0,0 +1,31 @@
+//
+//  CityWeatherResponse.swift
+//  Weather Forecast
+//
+//  Created by Mehrdad Ahmadian on 2021-09-06.
+//
+
+import Foundation
+
+struct CityWeatherResponse: Codable {
+    let main: Main
+    let name: String
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    let pressure: Int
+    let humidity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case humidity
+    }
+}
